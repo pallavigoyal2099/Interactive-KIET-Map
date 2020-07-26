6584ee4604e407a19de130005aba5e0c674ecdf8
Pallavi Goyal <pallavigoyal2099@gmail.com>
2020-07-26 22:40:28 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import { Map, GoogleApiWrapper, Marker} from 'google-maps-react';
+
+ 
+const mapStyles = {
+  width: '100%',
+  height: '100%',
+};
+ 
+ 
+function App(props) {
+  return (
+    <Map
+    styles={
+      [
+        { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+        { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+        { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+        {
+          featureType: "administrative.locality",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#d59563" }]
+        },
+        {
+          featureType: "poi",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#d59563" }]
+        },
+        {
+          featureType: "poi.park",
+          elementType: "geometry",
+          stylers: [{ color: "#263c3f" }]
+        },
+        {
+          featureType: "poi.park",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#6b9a76" }]
+        },
+        {
+          featureType: "road",
+          elementType: "geometry",
+          stylers: [{ color: "#38414e" }]
+        },
+        {
+          featureType: "road",
+          elementType: "geometry.stroke",
+          stylers: [{ color: "#212a37" }]
+        },
+        {
+          featureType: "road",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#9ca5b3" }]
+        },
+        {
+          featureType: "road.highway",
+          elementType: "geometry",
+          stylers: [{ color: "#746855" }]
+        },
+        {
+          featureType: "road.highway",
+          elementType: "geometry.stroke",
+          stylers: [{ color: "#1f2835" }]
+        },
+        {
+          featureType: "road.highway",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#f3d19c" }]
+        },
+        {
+          featureType: "transit",
+          elementType: "geometry",
+          stylers: [{ color: "#2f3948" }]
+        },
+        {
+          featureType: "transit.station",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#d59563" }]
+        },
+        {
+          featureType: "water",
+          elementType: "geometry",
+          stylers: [{ color: "#17263c" }]
+        },
+        {
+          featureType: "water",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#515c6d" }]
+        },
+        {
+          featureType: "water",
+          elementType: "labels.text.stroke",
+          stylers: [{ color: "#17263c" }]
+        }
+      ]
+    }
+    google={props.google}
+    zoom={19}
+
+    style={mapStyles}
+    initialCenter={ { lat: 28.752604, lng: 77.49793 }}
+    clickableIcons={true}
+  >
+    <Marker position={ { lat:  28.7525467, lng:  77.49793 }}
+    title={`welcome to Kiet group of institution`}
+    
+    />
+     
+    
+  </Map>
+  );
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyAYIAvpkn3_s38iYTVjKKR5otnm2FA9jvc'
+})(App);
